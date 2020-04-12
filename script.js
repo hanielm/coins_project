bd13a47058a61f20490259e855cf423dd2827aed
unknown <naormordechaix@gmail.com>
2020-04-12 21:21:58 +0300

    added modal

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var coins = [];
 var showMorInfoMapped = {};
 var favoritesCoins = [];
+var copyFavoritesCoins = [];
+
+var gCurrentCurrency;
+var gCurrentEl;
+
+//  $('.modal').addClass('in').css('display', 'block');
 
 
 $.get("https://api.coingecko.com/api/v3/coins", function (data) {
   console.log("Coins", { data });
   coins = data;
   $('.lds-ring').css('display', 'none');
-
   renderToHTML(coins);
 });
 
@@ -17,7 +22,7 @@ function renderToHTML(data) {
     strHtml += `<div class="box" id="id_${index}"> 
       <div class="symbol"> ${data[index].symbol} 
       ${`<div class="divSlider"><label class="switch"><input type="checkbox" 
-      onchange="sliderToggle(${index},event)"><span class="slider round"></span></label></div>`} </div>
+      onchange="sliderToggle(this,${index},event)"><span class="slider round"></span></label></div>`} </div>
        <div class="name"> ${data[index].name} </div>
        <div class="info"> <button type="button" onclick="getInfo(${index})" class="btn btn-primary">More info</button></div>
        <div class="lds-hourglass"></div>
@@ -32,13 +37,26 @@ function renderToHTML(data) {
 function getInfo(index) {
   $(`#id_${index} .lds-hourglass`).css('display', 'block');
   var id = coins[index].id;
-  if (showMorInfoMapped[index]) {
-    showMorInfoMapped[index] = false;
-    $(`#id_${index} .moreInfo`).remove();
+  if (showMorInfoMapped[index] && showMorInfoMapped[index].data) {
+    $(`#id_${index} .lds-hourglass`).css('display', 'none');
+    if (showMorInfoMapped[index].isActive) {
+      $(`#id_${index} .moreInfo`).remove();
+      showMorInfoMapped[index].isActive = false;
+    } else {
+      showMorInfoMapped[index].isActive = true;
+      $(`#id_${index}`).append(`<div class="moreInfo"> 
+    <div><img src="${showMorInfoMapped[index].data.image.small}"></div>
+    <div class="div_market">
+      <span class="Usd">Usd=  ${showMorInfoMapped[index].data.market_data.current_price.usd}$ </span>
+      <span class="Eur">Eur=  ${showMorInfoMapped[index].data.market_data.current_price.eur}€ </span>
+      <span class="Ils">Ils=  ${showMorInfoMapped[index].data.market_data.current_price.ils}₪ </span>
+    </div>
+    </div>`);
+    }
+
   } else {
     $.get(`https://api.coingecko.com/api/v3/coins/${id}`, function (data) {
       $(`#id_${index} .lds-hourglass`).css('display', 'none');
-      console.log({ data });
       $(`#id_${index}`).append(`<div class="moreInfo"> 
       <div><img src="${data.image.small}"></div>
       <div class="div_market">
@@ -47,7 +65,13 @@ function getInfo(index) {
         <span class="Ils">Ils=  ${data.market_data.current_price.ils}₪ </span>
       </div>
       </div>`);
-      showMorInfoMapped[index] = true;
+      showMorInfoMapped[index] = {
+        isActive: true,
+        data: data
+      };
+      setTimeout(() => {
+        showMorInfoMapped[index].data = null;
+      }, 20000); // after Two minutes clear CACHE and AJAX called again.
     });
   }
 }
@@ -81,31 +105,34 @@ function onChangeGraphCoins(dataPoints = []) {
 //
 
 
-function sliderToggle(index, ev) {
+function sliderToggle(el, index, el) {
   var dataPoints = [];
   var currency = coins[index];
-  var isCheked = ev.target.checked;
+  var isCheked = el.target.checked;
 
-  if (favoritesCoins.length >= 5 && isCheked) {
-    ev.target.checked = false;
+  if (favoritesCoins.length >= 1 && isCheked) {
+    drawerModalCoins(favoritesCoins);
+    gCurrentCurrency = currency;
+    gCurrentEl = el;
     return;
   }
 
   if (isCheked) {
-    favoritesCoins.push(currency.symbol);
+    favoritesCoins.push({ symbol: currency.symbol, isRemoved: false });
   } else {
-    favoritesCoins = favoritesCoins.filter(fCoin => fCoin !== currency.symbol);
+    favoritesCoins = favoritesCoins.filter(fCoin => fCoin.symbol !== currency.symbol);
   }
 
+  var symbols = favoritesCoins.map(coin => coin.symbol);
+
   $.get(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${favoritesCoins.join()}&tsyms=USD`,
+    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbols.join()}&tsyms=USD`,
     function (response) {
       for (const key in response) {
         if (response.hasOwnProperty(key)) {
           dataPoints.push({ label: key, y: response[key].USD });
         }
       }
-
       onChangeGraphCoins(dataPoints);
     }
   );
@@ -113,7 +140,69 @@ function sliderToggle(index, ev) {
 
 
 function onKeyDownHandler(event) {
-  if (event.keyCode === 13) { // on press ENTER in ketboard.
+  if (event.keyCode === 13) { // handle press ENTER in keyboard for search.
     onSearch()
   }
+}
+
+function drawerModalCoins(coins, newCurrency, ev) {
+  copyFavoritesCoins.length ? copyFavoritesCoins : copyFavoritesCoins = coins.map(coin => ({ ...coin }));
+  var strHtml = '';
+  for (let index = 0; index < copyFavoritesCoins.length; index++) {
+    var favoriteCoin = copyFavoritesCoins[index];
+
+    strHtml += `<div class="flex space-between">
+    <span class=${favoriteCoin.isRemoved ? 'line-throgh' : null}>${favoriteCoin.symbol}</span>
+    ${favoriteCoin.isRemoved ? `<button onclick='handleCoinsStatus(${index})'>Back to</button>` : `<button onclick="handleCoinsStatus(${index})">Remove</button>`}
+    </div>`
+  }
+
+  $('.modal').addClass('in').css('display', 'block');
+  console.log(strHtml);
+
+  $('.modal .modal-body').html(strHtml)
+
+
+}
+
+
+function handleCoinsStatus(index, ev) {
+  var requestedCoin = copyFavoritesCoins[index];
+  requestedCoin.isRemoved = !requestedCoin.isRemoved;
+  drawerModalCoins(copyFavoritesCoins)
+
+}
+
+function renderBodyModal() {
+
+}
+
+function onSubmit() {
+  favoritesCoins = [...copyFavoritesCoins];
+  favoritesCoins = favoritesCoins.filter(fCoin => !fCoin.isRemoved);
+  copyFavoritesCoins = [];
+  if (favoritesCoins.length <= 1) {
+    favoritesCoins.push({ symbol: gCurrentCurrency.symbol, isRemoved: false });
+    gCurrentEl.target.checked = true;
+    var symbols = document.querySelectorAll('.box .symbol');
+    symbols.forEach(symbol => {
+      var isExsist = favoritesCoins.find(f => f.symbol === symbol.innerText.trim())
+      debugger;
+      if (isExsist) {
+        symbol.querySelector('.switch input').checked  = false;
+      }
+      debugger;
+
+    })
+  } else {
+    gCurrentEl.target.checked = false;
+  }
+
+  gCurrentEl = null;
+  $('.modal').removeClass('in').css('display', 'none');
+}
+
+function onCloseModal() {
+  gCurrentEl.target.checked = false;
+  $('.modal').removeClass('in').css('display', 'none');
 }
